a9f07df9303c51618a66f8104dbe6125ba7813f0
moad6127 <moad6127@gmail.com>
2025-06-29 10:59:02 +0900

    Feat : SoulGame
    기본 캐릭터클래스 및 기본 맵 생성

---
+++ Unreal_SoulGame/Source/Unreal_SoulGame/Private/SoulCharacter.cpp
@@ -0,0 +1,29 @@
+
+
+#include "SoulCharacter.h"
+
+
+ASoulCharacter::ASoulCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ASoulCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASoulCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASoulCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
