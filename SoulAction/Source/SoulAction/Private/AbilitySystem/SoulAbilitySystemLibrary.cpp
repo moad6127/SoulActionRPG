95151d3bc3784fdcad8109ed8b0632cda66d65af
moad6127 <moad6127@gmail.com>
2025-08-12 09:49:32 +0900

    Feat : SoulGame
    AttributeMene WidgetController클래스 생성하기
    +
    프로젝트에서 사용할 BlueprintFunctionLibrary클래스 생성하기

---
+++ SoulAction/Source/SoulAction/Private/AbilitySystem/SoulAbilitySystemLibrary.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/SoulAbilitySystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/HUD/SoulHUD.h"
+#include "Controller/SoulPlayerState.h"
+#include "UI/WidgetController/SoulWidgetController.h"
+
+UOverlayWidgetController* USoulAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (ASoulHUD* SoulHUD = Cast<ASoulHUD>(PC->GetHUD()))
+		{
+			ASoulPlayerState* PS = PC->GetPlayerState<ASoulPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+			
+			const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+			return SoulHUD->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+
+	return nullptr;
+}
---
+++ SoulAction/Source/SoulAction/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/SoulWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULACTION_API UAttributeMenuWidgetController : public USoulWidgetController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BroadcastInitialValues() override;
+	virtual void BindCallbacksToDependencies() override;
+
+};
