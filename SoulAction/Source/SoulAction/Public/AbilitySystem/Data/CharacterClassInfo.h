03eef896b4b001ccf5e178cdd01a50a398a9d650
moad6127 <moad6127@gmail.com>
2025-09-12 08:03:57 +0900

    Feat : SoulGame
    캐릭터 클래스 데이터 에셋을 생성하기

---
+++ SoulAction/Source/SoulAction/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+/**
+ * 
+ */
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Elementalist,
+	Warrior,
+	Ranger
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category = "Class Defautls")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+
+};
+
+UCLASS()
+class SOULACTION_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Charcter Class Defautls")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInfomation;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defautls")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defautls")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
+
+};
