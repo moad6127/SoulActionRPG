e2ea9cc5842eec1b69aeee844ffa72d244101354
moad6127 <moad6127@gmail.com>
2025-09-24 11:06:32 +0900

    Feat : SoulGame
    데미지 계산 클래스에 Armor속성 캡쳐하기

--- SoulAction/Source/SoulAction/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ SoulAction/Source/SoulAction/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -3,9 +3,29 @@
 
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/SoulAttributeSet.h"
+
+struct SoulDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+
+	SoulDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(USoulAttributeSet, Armor, Target, false);
+	}
+
+};
+
+static const SoulDamageStatics& DamageStatics()
+{
+	static SoulDamageStatics DStatics;
+
+	return DStatics;
+}
 
 UExecCalc_Damage::UExecCalc_Damage()
 {
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
@@ -18,4 +38,16 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
 
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	FAggregatorEvaluateParameters EvaluationParmas;
+	EvaluationParmas.SourceTags = SourceTags;
+	EvaluationParmas.TargetTags = TargetTags;
+
+	float Armor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParmas, Armor);
+	Armor = FMath::Max<float>(0.f, Armor);
+
+	FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
