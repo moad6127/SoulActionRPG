63b628be08c0a73be5ca31014e422533098f337e
moad6127 <moad6127@gmail.com>
2025-12-06 17:03:06 +0900

    Feat : SoulGame
    SummonAbility로 소환된 Actor들이 파괴될때 minionCount를 줄이도록 만들기

--- SoulAction/Source/SoulAction/Private/AbilitySystem/Abilities/SoulProjectileSpell.cpp
+++ SoulAction/Source/SoulAction/Private/AbilitySystem/Abilities/SoulProjectileSpell.cpp
@@ -15,7 +15,7 @@ void USoulProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 
 }
 
-void USoulProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag)
+void USoulProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride)
 {
 	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer)
@@ -29,6 +29,10 @@ void USoulProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 			SocketTag);
 		//const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
 		FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+		if (bOverridePitch)
+		{
+			Rotation.Pitch = PitchOverride;
+		}
 		/*필요하면 나중에 제거*/
 		//Rotation.Pitch = 0.f;
 		/*필요하면 나중에 제거*/
@@ -36,6 +40,7 @@ void USoulProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 
 		SpawnTransform.SetLocation(SocketLocation);
 		SpawnTransform.SetRotation(Rotation.Quaternion());
+
 
 
 		ASoulProjectile* Projectile = GetWorld()->SpawnActorDeferred<ASoulProjectile>(
