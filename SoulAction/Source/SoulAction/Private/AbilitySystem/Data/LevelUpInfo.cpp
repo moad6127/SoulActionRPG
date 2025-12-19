6cf067087fe279115ef797915b2830e0393ec69c
moad6127 <moad6127@gmail.com>
2025-12-19 14:07:28 +0900

    Feat : SoulGame
    Levelup에 필요한 XP와 Reward를 설정하기 위해 dataAsset클래스 생성하기

---
+++ SoulAction/Source/SoulAction/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level{ 1 };
+	bool bSearching = true;
+	while (bSearching)
+	{
+		if (LevelUpInfomation.Num() - 1 <= Level)
+		{
+			return Level;
+		}
+
+		if (XP >= LevelUpInfomation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
---
+++ SoulAction/Source/SoulAction/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSoulLevelUpInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 LevelUpRequirement = 0;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 AttributePointAward = 1;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpellPointAward = 1;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class SOULACTION_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FSoulLevelUpInfo> LevelUpInfomation;
+
+	int32 FindLevelForXP(int32 XP);
+};
